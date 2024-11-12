4f6cff1b3ab17681241d0e6cf32341f3319960ab
RuiFilipeGoncalvesPereira <rui6.319@hotmail.com>
2024-11-12 15:06:59 +0000

    Get Game by id

--- src/main/java/com/ruipereira/dslist/controllers/GameController.java
+++ src/main/java/com/ruipereira/dslist/controllers/GameController.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ruipereira.dslist.dto.GameMinDTO.GameDTO;
 import com.ruipereira.dslist.dto.GameMinDTO.GameMinDTO;
-import com.ruipereira.dslist.entities.Game;
 import com.ruipereira.dslist.services.GameService;
 
 @RestController
@@ -21,9 +22,14 @@ public class GameController {
 	@GetMapping
 	public List<GameMinDTO> findAll()
 	{
-		
 		List<GameMinDTO> result = gameService.findAll();
 		return result;
 	}
 	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id)
+	{
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
 }
